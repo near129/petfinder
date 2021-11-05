c499d78ffd459a38b1d2df4893ada81b151164d1
near129 <>
2021-11-05 22:35:52 +0900

    Fix lr warmup

--- petfinder/lr_schedulers/lr_warmup.py
+++ petfinder/lr_schedulers/lr_warmup.py
@@ -4,6 +4,8 @@ def create_warmup_lr(optimizer, lr_scheduler, lr_warmup_decay=0.1, lr_warmup_epo
     warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
         optimizer, start_factor=lr_warmup_decay, total_iters=lr_warmup_epochs
     )
-    return torch.optim.lr_scheduler.SequentialLR(
+    lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
         optimizer, schedulers=[warmup_lr_scheduler, lr_scheduler], milestones=[lr_warmup_epochs]
     )
+    lr_scheduler.optimizer = optimizer  # pytorch-lightningで必要なため
+    return lr_scheduler
